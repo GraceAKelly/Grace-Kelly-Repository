dcde94ae7eb50878ad30225ae3b296bc58f17c4c
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-02-11 14:10:42 +0000

    Create Tuesday.py

---
+++ Tuesday.py
@@ -0,0 +1,9 @@
+# Grace Kelly 06-02-18
+# Week 3 is it Tuesday
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+  print("yay! It is Tuesday")
+else:
+  print("unfortunetly it is not Tuesday")
