3ec04aaa64e2eafcf3e59b91f37a34daab8775a7
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-02-11 15:31:24 +0000

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,11 @@
+#Grace Kelly 11-02-2018
+#Week 3 Assesment 
+
+num = int(input("Please Enter Integer:"))
+while num!=1:
+    if num % 2 == 0: 
+        num = num/2
+        print (num)
+    else:
+        num = (num * 3) + 1
+        print (num) 
