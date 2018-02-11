e1eb7dfbc820963978fd69c2f111e935da05fb0a
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-02-11 16:04:42 +0000

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,5 +1,9 @@
-#Grace Kelly
-# Week 2 Assesment 30-01-2018
+#Grace Kelly 30-01-2018
+# Week 1 exercise My name is Grace, so the first and last letter of my name (G + E = 7 + 5) give the number 12. 
+# The 12th Fibonacci number is 144
+# Week 2 exercise: My surname is Kelly The first letter K is number 75 
+# The last letter y is number 121 Fibonacci number 196 is 40934782466626840596168752972961528246147
+# ord() is a built in function in python that returns the unique numeric value assigned in unicode for the given character input
 
 
 def fib(n):
