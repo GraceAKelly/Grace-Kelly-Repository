6d1b7c7cddf60008558bacd5cfe068ab4a7c0341
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-04-05 21:19:14 +0100

    Update multiplesof35.py

--- multiplesof35.py
+++ multiplesof35.py
@@ -1,11 +1,14 @@
-#Grace Kwelly
+#Grace Kelly
 #https://projecteuler.net/problem=1
 
-i = 1
-while i >1000:
-  if i % 3 == 0:
-  sum = sum + i
-  else i % 5 == 0:
-  sum = sum + if
-  i = i + 1
-print (sum is equal to, sum)
+#list all the natural numbers below 10 
+#that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+#The sum of these multiples is 23.
+
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+x = 1
+for i in range(1000): # all natural numbers below 1000
+  if i % 3 == 0 or i % 5 == 0: # is is divisable by 3 or 5
+    x += i
+print (x)
