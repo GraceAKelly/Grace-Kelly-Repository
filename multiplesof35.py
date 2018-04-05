04c41aef86b2dfb6fdeec18e50eaeae055065f54
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-04-05 20:57:21 +0100

    Create multiplesof35.py

---
+++ multiplesof35.py
@@ -0,0 +1,11 @@
+#Grace Kwelly
+#https://projecteuler.net/problem=1
+
+i = 1
+while i >1000:
+  if i % 3 == 0:
+  sum = sum + i
+  else i % 5 == 0:
+  sum = sum + if
+  i = i + 1
+print (sum is equal to, sum)
