887d2570a1c928140f26b67176e32a93304859bc
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-04-05 22:08:06 +0100

    Update Tuesday.py

--- Tuesday.py
+++ Tuesday.py
@@ -1,9 +1,9 @@
 # Grace Kelly 06-02-18
 # Week 3 is it Tuesday
 
-import datetime
+import datetime # functions to loop and determine if the day of the week is Tuesday
 
-if datetime.datetime.today().weekday() == 1:
+if datetime.datetime.today().weekday() == 1: # Tuesday is eqaul to 1 in python
   print("yay! It is Tuesday")
 else:
   print("unfortunetly it is not Tuesday")
