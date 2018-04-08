6742b71c935fab41dd09e7e925d99f3915d21e76
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-04-08 10:08:17 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -4,8 +4,8 @@
 num = int(input("Please Enter Integer:")) # Enter number to be tested
 while num!=1: # Number is not equal to 1
     if num % 2 == 0: # If the remainder of dividing by 2 is 0 
-        num = num/2 
-        print (num)
-    else:
-        num = (num * 3) + 1
-        print (num) 
+        num = num/2 # If it is even divide by 2
+        print (num) # Print the output 
+    else: # Or Else if it is not even 
+        num = (num * 3) + 1 # Multiply the number by 3
+        print (num) #Print the output
