eb67d7e5888df90ad49347352a67f0aba2947850
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-04-08 10:08:52 +0100

    Update Tuesday.py

--- Tuesday.py
+++ Tuesday.py
@@ -6,4 +6,4 @@ import datetime # functions to loop and determine if the day of the week is Tues
 if datetime.datetime.today().weekday() == 1: # Tuesday is eqaul to 1 in python
   print("yay! It is Tuesday") # output if it is true today is Tuesday
 else:
-  print("unfortunetly it is not Tuesday") # output if it is false today i snot Tuesday
+  print("unfortunetly it is not Tuesday") # output if it is false today is not Tuesday
