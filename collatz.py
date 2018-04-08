99fc95a6c29847e636a36a35d679a155dee6e8f5
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-04-08 10:05:34 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,10 +1,10 @@
 #Grace Kelly 11-02-2018
 #Week 3 Assesment 
 
-num = int(input("Please Enter Integer:"))
-while num!=1:
-    if num % 2 == 0: 
-        num = num/2
+num = int(input("Please Enter Integer:")) # Enter number to be tested
+while num!=1: # Number is not equal to 1
+    if num % 2 == 0: # If the remainder of dividing by 2 is 0 
+        num = num/2 
         print (num)
     else:
         num = (num * 3) + 1
