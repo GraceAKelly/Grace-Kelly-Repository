6a9ca3b9413d0734b795527fcbc1bd032b4cdfcf
GraceAKelly <36331374+GraceAKelly@users.noreply.github.com>
2018-04-08 10:56:11 +0100

    Create sumultiply.py

---
+++ sumultiply.py
@@ -0,0 +1,14 @@
+# Grace Kelly 
+# 08 March 2018
+
+# Practice problem 1
+# Use summultiple function
+# # https://github.com/ianmcloughlin/problems-python-fundamentals
+
+def sumultiply(x, y): # Define optput required
+total = 0  
+for i in range(y):
+total = total + x # Change definition of total Loop y and add x to total
+return total
+print(sumultiply(11, 13))
+print(sumultiply(5, 123))
